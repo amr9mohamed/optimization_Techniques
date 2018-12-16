de3b7f76f3dcd111b1fbe8da99dcfa8b6c1de653
alyswidan <swidan12340@gmail.com>
2018-12-16 21:52:44 +0200

    created functions for testing

--- penalty_method.py
+++ penalty_method.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-class Differentiable_fn():
+class DifferentiableFn():
     def gradient(self):
         pass
 
-class Differentiable_square(Differentiable_fn):
+class DifferentiableSquare(DifferentiableFn):
     def __init__(self, fn):
         self.fn = fn
 
@@ -14,7 +14,7 @@ class Differentiable_square(Differentiable_fn):
     def gradient(self, vec):
         return 2*self.fn(vec)
 
-class Differentiable_max(Differentiable_fn):
+class DifferentiableMax(DifferentiableFn):
     def __init__(self, fn1, fn2):
         self.fn1 = fn1
         self.fn2 = fn2
@@ -28,7 +28,7 @@ class Differentiable_max(Differentiable_fn):
         
         return fn1_mask * self.fn1.gradient(vec) + fn2_mask * self.fn2.gradient(vec)
 
-class Differentiable_add(Differentiable_fn):
+class DifferentiableAdd(DifferentiableFn):
     def __init__(self, fn1, fn2):
         self.fn1 = fn1
         self.fn2 = fn2
@@ -40,7 +40,7 @@ class Differentiable_add(Differentiable_fn):
         return self.fn1.gradient(vec) + self.fn2.gradient(vec)
 
 
-class Differentiable_mult(Differentiable_fn):
+class DifferentiableMult(DifferentiableFn):
     def __init__(self, fn1, fn2):
         self.fn1 = fn1
         self.fn2 = fn2
@@ -51,7 +51,7 @@ class Differentiable_mult(Differentiable_fn):
     def gradient(self, vec):
         return self.fn1.gradient(vec) * self.fn2(vec) + self.fn1.gradient(vec) * self.fn2(vec)
 
-class Differentiable_constant(Differentiable_fn):
+class DifferentiableConstant(DifferentiableFn):
     def __init__(self, value):
         self.value = value
 
@@ -71,46 +71,88 @@ class Differentiable_constant(Differentiable_fn):
 
 
 class PentaltyOptimizer:
+    
+    def __init__(self, speed_up_every=1, c=10, lr=1e-1):
+        self.speed_up_every = speed_up_every
+        self.c = c
+        self.lr = lr
+
+
     def optimize(self, fn, equality_constraints,
-                 inequality_constraints, initial_vec, steps,
-                 speed_up_every=1, c=10, lr=1e-1):
+                 inequality_constraints, initial_vec, steps):
 
         def _constraints_satisfied(vec):
             return all([constraint(vec) == 0 for constraint in equality_constraints]) and \
                    all([constraint(vec) <= 0 for constraint in inequality_constraints])
 
 
-        r_k = Differentiable_constant(1)
+        r_k = DifferentiableConstant(1)
 
-        zero = Differentiable_constant(0)
+        zero = DifferentiableConstant(0)
 
         equality_constraints_fn = zero
 
         for constraint in equality_constraints:
-            equality_constraints_fn = Differentiable_add(equality_constraints_fn, 
-                                                         Differentiable_square( constraint) ) 
+            equality_constraints_fn = DifferentiableAdd(equality_constraints_fn, 
+                                                         DifferentiableSquare( constraint) ) 
 
         inequality_constraints_fn = zero
         
         for constraint in inequality_constraints:
-            sq_max = Differentiable_square(Differentiable_max(constraint, zero))
-            inequality_constraints_fn = Differentiable_add(inequality_constraints_fn, sq_max)
+            sq_max = DifferentiableSquare(DifferentiableMax(constraint, zero))
+            inequality_constraints_fn = DifferentiableAdd(inequality_constraints_fn, sq_max)
                                                            
 
-        equality_constraints_fn = Differentiable_mult(r_k, equality_constraints_fn)
+        equality_constraints_fn = DifferentiableMult(r_k, equality_constraints_fn)
 
-        inequality_constraints_fn = Differentiable_mult(r_k, inequality_constraints_fn)
+        inequality_constraints_fn = DifferentiableMult(r_k, inequality_constraints_fn)
 
-        constraints_fn = Differentiable_add(inequality_constraints_fn, equality_constraints_fn)
+        constraints_fn = DifferentiableAdd(inequality_constraints_fn, equality_constraints_fn)
     
-        penalty_fn = Differentiable_add(fn, constraints_fn)
+        penalty_fn = DifferentiableAdd(fn, constraints_fn)
 
         curr_vec = initial_vec
 
-        for _ in range(steps):
+        for step in range(steps):
             penalty_gradient = penalty_fn.gradient(curr_vec)
-            curr_vec = curr_vec - penalty_gradient
+            curr_vec -= self.lr * penalty_gradient
+            
+            if _constraints_satisfied(curr_vec):
+                return curr_vec
 
+            if step % self.speed_up_every == 0 and step != 0:
+                r_k *= self.c
+
+            
+class CustomFn(DifferentiableFn):
+
+    def __call__(self, vec):
+        return (1/3) * (1 + vec[0])**3  + vec[1]
+
+    def gradient(self, vec):
+        return (1 + vec[0]) ** 2 + 1
+
+class InequalityConstraint1(DifferentiableFn):
+    
+    def __call__(self, vec):
+        return 1-vec[0]
+
+    def gradient(self, vec):
+        return -1
+
+class InequalityConstraint2(DifferentiableFn):
+    
+    def __call__(self, vec):
+        return -vec[1]
+
+
+    def gradient(self, vec):
+        return -1
+
+def main():
+    optimizer = PentaltyOptimizer()
+
+    optimizer.minimize()
 
 
 
