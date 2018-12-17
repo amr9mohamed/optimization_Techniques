7bde6d536118897bfe321a5634952e095ba43cd3
amr9mohamed <amrmohamed53@gmail.com>
2018-12-17 21:25:03 +0200

    generalizing the code by adding a neigbour function

--- SA.py
+++ SA.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 def func(X):
     return 1 / (1 + X[0]**2 + X[1]**2)
 
-def acceptance_probability(old, new, T):
-    return np.exp(-np.sum(new - old) / T)
+def acceptance_probability(old_cost, new_cost, t):
+    return np.exp(-np.sum(new_cost - old_cost) / t)
 
-def anneal(sol):
+def neighbor(sol, T0, Ti, lower, upper):
+    return sol *(1 - Ti/T0 ) + (Ti/T0) * random.uniform(lower, upper)
+
+def anneal(sol, lower, upper):
     old_cost = func(sol)
     T0 = 1.0
     T_min = 0.00001
@@ -16,9 +19,10 @@ def anneal(sol):
     Ti = T0
     best_sol = sol
     best_cost = old_cost
+    
     while Ti > T_min:
         for _ in range(100):
-            new_sol = sol *(1 - Ti/T0 ) + (Ti/T0) * random.uniform(-5, 5)
+            new_sol = neighbor(sol, T0, Ti, lower, upper)
             new_cost = func(new_sol)
             if new_cost > old_cost:
                 sol = new_sol
@@ -30,12 +34,13 @@ def anneal(sol):
                 if ap > np.random.random():
                     sol = new_sol
                     old_cost = new_cost
-
-        Ti = Ti*alpha
-
+        
+        Ti = alpha*Ti
+            
     return best_sol, best_cost
 
-X = np.array([(-1), (-1)])
-sol, cost = anneal(X)
-print(sol, cost)
-
+lower = -100
+upper =  100
+X = np.random.randint(low=lower, high=upper, size=(2))
+sol, cost = anneal(X, lower=lower, upper=upper)
+print(f'Best solution is {sol}, function at this point is {cost}')
